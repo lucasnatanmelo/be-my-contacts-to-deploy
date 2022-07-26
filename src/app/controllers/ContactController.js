76b1ce923b0a46fea370fce59cf4083759ca0b91
Lucas Natan de Almeida Melo <lucasnatan.dev@gmail.com>
2022-07-26 16:20:34 -0400

    --------

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -20,9 +20,9 @@ class ContactController {
   async show(request, response) {
     const { id } = request.params;
 
-    if (!isValidUUID(id)) {
-      return response.status(400).json({ error: 'Invalid contact id' });
-    }
+    // if (!isValidUUID(id)) {
+    //   return response.status(400).json({ error: 'Invalid contact id' });
+    // }
 
     const contact = await ContactsRepository.findById(id);
 
